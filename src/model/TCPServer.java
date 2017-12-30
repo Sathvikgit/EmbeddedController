2860f4753b6bedd28cf804b9bf012d1ad964d30b
gsath <gsath@DESKTOP-3SANJ1K>
2017-12-29 21:50:34 -0800

    TCP server gui added

--- src/model/TCPServer.java
+++ src/model/TCPServer.java
@@ -1,10 +1,18 @@
 package model;
 
+import embeddedcontroller.EmbeddedController;
 import static embeddedcontroller.EmbeddedController.DEBUG;
+import static embeddedcontroller.EmbeddedController.hs;
+import static embeddedcontroller.EmbeddedController.myTCPServer;
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import systemDefinitions.logPacket;
 
 
 
@@ -18,12 +26,17 @@ public class TCPServer implements Runnable{
     public BufferedReader reader = null;
     public BufferedWriter writer = null;
     
+    //log all the packets to this list 
+    public List<logPacket> rxPackets = new ArrayList<>(); 
+    public DefaultTableModel TCPserverModel = (DefaultTableModel) hs.TCPServerLog.getModel();
+    
     public TCPServer(int port, int maxClient) {
         this.serverPort = port;
         this.maxClient = maxClient;
         DEBUG.log(Level.INFO, "Creating TCPserver: {0}", port);
         try {
              sock = new ServerSocket(port);
+             rxPackets.clear();
              // start listening on new thread
              new Thread(this).start();
           
@@ -36,55 +49,64 @@ public class TCPServer implements Runnable{
     @Override
     public void run() {
         while (true) {            
+            DEBUG.log(Level.INFO, "listening...");
             try {
-                DEBUG.log(Level.INFO, "listening...");
+                
                 this.client = sock.accept();
                 DEBUG.log(Level.INFO, " {0} Connected", client.getInetAddress());
-                try {
-                    reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                    writer =new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-                    
-                    // send welcome message 
-                    writer.write("*** Embedded TCP Server ***\r\n");            
-		    writer.write("*** Quit -> close connection *** \r\n");
-		    writer.flush();
-		    String cmd = "";
-                    boolean closeClient = false;
-                    do{
-                        if(client.isClosed() || !client.isConnected()){
+                
+                reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                // we are not writing any thing
+                writer =new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                
+                // send welcome message
+                //writer.write("*** Embedded TCP Server ***\r\n");
+                //writer.write("*** Quit -> close connection *** \r\n");
+                //writer.flush();
+                String cmd = "";
+                boolean closeClient = false;
+                do{
+                    if(client.isClosed() || !client.isConnected()){
+                        closeClient = true;
+                        DEBUG.log(Level.INFO, "Client socket Closed");
+                    }else{
+                        try{
+                            cmd = reader.readLine().trim();
+                            // log this packet
+                            logPacket lp = new logPacket(new Date(),
+                                    ((InetSocketAddress)client.getRemoteSocketAddress()).getAddress().toString().replace("/",""),
+                                    client.getPort(),
+                                    client.getLocalAddress().toString().replace("/",""),
+                                    client.getLocalPort(),
+                                    "", 
+                                    cmd);
+                            // log this packet 
+                             tcpServerlogPacket(lp);
+                             
+                        }catch(Exception e){
                             closeClient = true;
-                            DEBUG.log(Level.INFO, "Client socket Closed");
-                        }else{
-                            try{
-                                cmd = reader.readLine().trim();
-                                 if("Quit".equals(cmd)){
-                                    writer.write("Closing Connection.. \r\n");
-                                    writer.flush();
-                                    client.close();
-                                }else{
-                                    // process the commands
-                                    writer.write("Echo>"+cmd+"\r\n"); 
-                                    writer.flush();
-                                }
-                            }catch(Exception e){
-                                closeClient = true;
-                                DEBUG.log(Level.INFO, "Connection Terminated by client");
-                                client.close();
-                            }
+                            DEBUG.log(Level.INFO, "Client Disconnected {0}", e.getMessage());
+                            client.close();
                         }
-                        
-                    }while(!closeClient);
+                    }
                     
-                } catch (IOException e) {
-                    Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, e);
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+                }while(!closeClient);
+                
+            } catch (IOException e) {
+                Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, e);
             }
         }
     }
 
-    
+ public void stop(){
+     
+ }   
+ private static void tcpServerlogPacket(logPacket lp){
+    myTCPServer.rxPackets.add(lp);
+    // add this packet to the table 
+    myTCPServer.TCPserverModel.addRow(lp.getRowData());
+    System.out.println(lp.toString());
+ }    
 
     
 }
