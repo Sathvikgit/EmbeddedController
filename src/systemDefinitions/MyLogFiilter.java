df0b33f061a71a675abc38234ad70474697c067f
gsath <gsath@DESKTOP-3SANJ1K>
2018-01-03 14:41:32 -0800

    Logger Updated

--- src/systemDefinitions/MyLogFiilter.java
+++ src/systemDefinitions/MyLogFiilter.java
@@ -2,6 +2,7 @@ package systemDefinitions;
 
 import embeddedcontroller.EmbeddedController;
 import static embeddedcontroller.EmbeddedController.DEBUG;
+import java.util.ArrayList;
 import java.util.logging.Filter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -9,6 +10,20 @@ import java.util.logging.LogRecord;
 public class MyLogFiilter implements Filter{
 
     public static boolean StopLogging = false;
+
+    // class in this list will not be logged
+    public static ArrayList<String> filterClasses;
+
+    public MyLogFiilter() {
+        filterClasses = new ArrayList<>();
+        
+        // Filter the following Source classes
+        filterClasses.add("systemDefinitions.responding_network_devices");
+        filterClasses.add("systemDefinitions.process_packet");
+        
+        DEBUG.log(Level.INFO,"Log Filters:"+ filterClasses.toString()+"\r\n");
+    }
+    
     
     // Check if a given log record should be published.
     // return false: Will not be logged
@@ -16,10 +31,13 @@ public class MyLogFiilter implements Filter{
     public boolean isLoggable(LogRecord record) {
         if(StopLogging){
             return false;
-        }else{
-        
-            return true;
         }
+        
+        if(filterClasses.contains(record.getSourceClassName())){
+            return false;
+        }
+        
+         return true;
     }
     
     // Use this methd to set the log levels
--- src/systemDefinitions/MyLogFormatter.java
+++ src/systemDefinitions/MyLogFormatter.java
@@ -7,14 +7,13 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
+import static systemDefinitions.MyLogFiilter.filterClasses;
 
 
 public class MyLogFormatter extends Formatter{
 
     private static SimpleDateFormat localDateFormat = new SimpleDateFormat("HH:mm:ss");
-    
    
-    
     @Override
     public String format(LogRecord record) {
         String message=record.getMessage();
@@ -30,12 +29,17 @@ public class MyLogFormatter extends Formatter{
                                 +"Log Date:"+ new Date().toString()+"\r\n"+
                                 "\r\n-------------------------------------------------\r\n"; 
             message = logBanner+"\r\n"+message;
+                                                   
         }
         
-        return localDateFormat.format(new Date(record.getMillis()))+"  <"+record.getLevel()+">  "+
+        message = localDateFormat.format(new Date(record.getMillis()))+"  <"+record.getLevel()+">  "+
                             record.getSourceClassName()+
                             "("+record.getSourceMethodName()+"):   " 
                             +message+"\r\n";
+        
+        // log the messages to the console beacsue you disabel the console log handler to the logger
+        System.out.println(message);
+        return message;
     }
     
 }
